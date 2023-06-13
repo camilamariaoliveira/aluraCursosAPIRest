df1ff51b4a4def40f3062d0dc9307307f254e788
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-06-13 19:47:55 -0300

    Spring Boot 3: aplique boas práticas e proteja uma API Rest(Completo)

---
+++ src/main/java/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente (Long id, String nome, String telefone, @Valid DadosEndereco endereco){
+}
--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.paciente;
-
-public record DadosListagemPaciente(String nome, String email, String cpf) {
-    public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
-    }
-}
